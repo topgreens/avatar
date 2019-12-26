ad5dcabb361300f04b9ad41215557455b2ab0b6a
topgreens <topgreens@mail.com>
2019-12-26 05:48:23 -0600

    avatar

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+functioncolorpiel() {
+    var color = document.getElementById("color-piel".value);
+    document.getElementById("fondo-avatar").style.backgroundColor = color;
+}
+
+function ponerPelo() {
+    var pelo = document.createElement("div");
+    pelo.setAttribute();
+}
+
+Function quitarPelo() {
+    document.getElementById().style.display = none;
+}
